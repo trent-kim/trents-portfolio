fdba0e5b4292056f88c9fb3f1d75d78ab25f692f
Trent Lowe <trentrlowe@gmail.com>
2023-04-06 20:16:04 -0400

    Update Seo, add favicon.ico and share.png + add sitemap using next-sitemap

--- frontend/components/Seo.js
+++ frontend/components/Seo.js
@@ -13,16 +13,16 @@ const Seo = () => {
       <meta itemProp="name" content="Trent Kim"/>
       <meta name="application-name" content="Trent Kim"/>
       <meta name="og:site_name" content="Trent Kim"/> 
-      <link rel="icon" type="image/png" href='https://trentkim.com/favicon.ico'/>
-      <meta property="og:image" href="https://trentkim.com/share.png"/>
-      <meta property="og:image:secure_url" content="https://trentkim.com/share.png" /> 
+      <link rel="icon" type="image/png" href='https://trentkim.space/favicon.ico'/>
+      <meta property="og:image" href="https://trentkim.space/share.png"/>
+      <meta property="og:image:secure_url" content="https://trentkim.space/share.png" /> 
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="I'm web designer and developer currently based in my hometown, Pittsgrove, NJ." />
       <meta property="og:description" content="I'm web designer and developer currently based in my hometown, Pittsgrove, NJ."/>  
       <meta property="og:locale" content="en_US"/>
       <meta property="og:type" content="website"/>
-      <meta property="og:url" content="https://trentkim.com"/> 
-      <link rel="canonical" href="https://trentkim.com"/> 
+      <meta property="og:url" content="https://trentkim.space"/> 
+      <link rel="canonical" href="https://trentkim.space"/> 
       <meta name="theme-color" content="#F5F5F5"/> 
       <meta property="og:image:alt" content="Trent Kim"/> 
       <meta name="robots" content="index,follow"/>
